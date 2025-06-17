4dc8f2f15bc0752f9894064ab0af2ea10c9d25dc
jensine04 <jensine.soji@gmail.com>
2025-06-17 15:16:04 +0530

    .

--- src/components/engineerDashboardLayout/EngineerDashboardLayout.tsx
+++ src/components/engineerDashboardLayout/EngineerDashboardLayout.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import EngineerDashboardSidebar from "../sidebar/EngineerDashboardSidebar";
 import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import { Navigate } from "react-router-dom";
 
 const EngineerDashboardLayout = () => {
+
+  const isLoggedIn= () =>{
+    const token= localStorage.getItem("token")
+    if(token)
+      return true
+    else
+    return false
+    
+  };
+
+  if(!isLoggedIn()){
+     return <Navigate to='/login'/>
+  }
+
   return (
     <div className="flex">
       <SidebarProvider>
