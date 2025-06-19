2b0be70435f21bb3a9e15e431b53fc4361d419ba
aaron <aaronjacobaj4@gmail.com>
2025-06-19 11:32:43 +0530

    Implemented Engineer Details page

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,8 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import EngineerProfile from "./pages/engineer/profile/EngineerProfile";
 import HrProjectDetails from "./pages/hr/hrProjectDetails/HrProjectDetails";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
+import EngineerDetails from "./pages/hr/engineers/EngineerDetails";
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,7 @@ const router = createBrowserRouter([
       { path: "projects", element: <Projects /> },
       { path: "projects/:id", element: <HrProjectDetails /> },
       { path: "engineers", element: <Engineers /> },
+      {path: "engineers/:id", element: <EngineerDetails /> },
       { path: "alerts", element: <Alerts /> },
       { path: "history", element: <History /> },
       { path: "profile", element: <Profile /> },
